171eae6fcdc24bf6503cb47748a3c9a3381eb710
jorge fischer <jorgefisde@gmail.com>
2019-09-09 17:20:25 -0600

    calis comit

--- routes/login.js
+++ routes/login.js
@@ -14,7 +14,7 @@ app.post('/', (req, res) => {
     var email = emp.email
     var contraseña = emp.contraseña
     const sql = "SELECT id,nombre,apellido,email,fecha_Nacimiento FROM Usuario WHERE email = ? AND contraseña = sha1(?)"
-    if (email && contraseña) {
+    //if (email && contraseña) {
 
             baseDatos.query(sql, [email, contraseña], (err, results) => {
                 if (results.length > 0) {
@@ -34,10 +34,10 @@ app.post('/', (req, res) => {
                 }
             })
         
-    } else {
-        res.send('Por favor inserta un email y un correo')
+  //  } else {
+    //    res.send('Por favor inserta un email y un correo')
         res.end()
-    }
+   // }
 
 })
 
