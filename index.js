9848fbc12e26146ae635e08b5e387eae3706be92
jorge fischer <jorgefisde@gmail.com>
2019-09-12 17:02:18 -0600

    getAcounts commit

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const app = express()
 
 app.set('port', process.env.PORT || 3000)
 
-const Register = require('./routes/register');
-const getCuenta = require('./routes/crearCuenta')
+const Register = require('./routes/register')
+const cuenta = require('./routes/crearCuenta')
+const getAcount = require('./routes/acount')
 const login = require('./routes/login')
 const home = require("./routes/index")
 const transac = require('./routes/transaccion')
@@ -15,7 +16,8 @@ app.use(express.urlencoded({ extended: true }))
 
 
 app.use('/api/register',Register)
-app.use('/api/createAcounts',getCuenta)
+app.use('/api/createAcounts',cuenta)
+app.use('/api/acounts',getAcount)
 app.use('/api/login',login.app)
 app.use('/api/home',home)
 app.use('/api/transaccion',transac)
--- routes/acount.js
+++ routes/acount.js
@@ -6,9 +6,9 @@ const baseDatos = require('../servicio/conexionBD')
 const datosLogin = require('./login')
 
 router.get("/", datosLogin.verificscionToken, (req, res, next) => {
-    var sql = "SELECT * FROM Cuenta where id_noCuenta = ? AND id_usuario = ?"
+    var sql = "SELECT * FROM Cuenta where idAND id_usuario = ?"
     jwt.verify(req.token, 'my_secret_key', (err, data) => {
-        baseDatos.query(sql,[req.body.noCuenta,data.user.id], (err, rows) => {
+        baseDatos.query(sql,[data.user.id], (err, rows) => {
             if (err) {
                 console.log(err);
                 res.send("Hubo un error")
