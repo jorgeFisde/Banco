df45209eb61c3d63ac402cda83c8acd95e3ca815
jorge fischer <jorgefisde@gmail.com>
2019-10-24 16:34:55 -0600

    arrglis x

--- routes/index.js
+++ routes/index.js
@@ -6,13 +6,15 @@ const baseDatos = require('../servicio/conexionBD')
 const datosLogin  = require('./login')
 
 router.get('/',datosLogin.verificscionToken, (req, res) => {
+    var sql = "SELECT * FROM Cuenta,cuentas_frecuentes WHERE Cuenta.id_usuario = ? AND cuentas_frecuentes.id_usuario = ?"
     jwt.verify(req.token, 'my_secret_key',(err,data)=>{
+        var myId = data.user.id
 
         if (err) {
             res.sendStatus(403)
         }else{
            
-            baseDatos.query("SELECT * FROM Cuenta WHERE id_usuario = ? ",[data.user.id], (err, rows) => {
+            baseDatos.query(sql,[myId,myId], (err, rows) => {
                 data.user.cuenta = rows
                 if (err) {
                     console.log(err)
